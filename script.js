8436bd7423f21020959729f6e50c8b0094e7e826
Gustavo Souza <gguvictor909@gmail.com>
2021-11-15 09:39:08 -0300

    Add dynamic squares

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const page_body = window.document.body
 button_submit.addEventListener('click', (event) => {
     event.preventDefault();
 
-    //validate fields
+    //validate form fields
     const form_fields = [...window.document.querySelectorAll('#login-form input')]; 
     form_fields.forEach(field => {
         if(field.value === ''){
@@ -29,15 +29,45 @@ button_submit.addEventListener('click', (event) => {
     }
 });
 
+/*when the 'down' animation starts, the body will have hidden overflow*/
 page_body.addEventListener('animationstart', (event) =>{
     if(event.animationName === 'down'){
         page_body.style.overflow = 'hidden'; 
     }
 });
 
+/*when the 'down' animation ends, the body will have none overflow and form will have display = 'none'*/
 main_form.addEventListener('animationend', (event) => {
     if(event.animationName === 'down'){
         main_form.style.display = 'none';
         page_body.style.overflow = 'none';
     }
 });
+
+/*background squares*/
+const ulSquares = window.document.querySelector('.squares');
+
+/*loop repeat to make the animation 'up' dynamic*/ 
+for (let i = 0; i < 21; i++) {
+    const li = document.createElement('li');   
+    
+    //random function 
+    const randomFunction = (min, max) => Math.random() * (max - min) + min;
+    
+    //dynamic settings
+    let size = Math.floor(randomFunction(10, 120));
+    let position = randomFunction(1, 99);
+    let delay = randomFunction(5, 0.1);
+    let duration = randomFunction(24, 12);
+
+    li.style.width = `${size}px`;
+    li.style.height = `${size}px`;
+    li.style.bottom = `-${size}px`;
+    li.style.left = `${position}%`;
+    li.style.animationDelay = `${delay}s`;
+    li.style.animationDuration = `${duration}s`;
+    li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`;
+
+    ulSquares.appendChild(li);
+}
+
